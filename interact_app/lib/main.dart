8129bfe480fe3c29052fbccf76d2b1f40b34e83f
zhoupengzu <zhoupengzu@maizijf.com>
2018-04-03 17:24:31 +0800

    修改展示内容

--- interact_app/lib/main.dart
+++ interact_app/lib/main.dart
@@ -3,107 +3,111 @@ import 'package:flutter/material.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.grey,
+    // TODO: implement build
+    return new _MyHomeWidget();
+  }
+}
+
+class _MyHomeWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new _MyHomeState();
+  }
+}
+
+class _MyHomeState extends State<_MyHomeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    // 图片
+    Image image = new Image.asset("images/lake.jpg");
+    // 文字
+    Widget titleSection = new Container(
+      padding: const EdgeInsets.all(32.0),
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          new Expanded(
+            child: new Column(
+              children: <Widget>[
+                new Container(
+                  margin: const EdgeInsets.only(bottom: 8.0),
+                  child: new Text(
+                    "Oeschinen Lake Campground",
+                    style: new TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                new Text(
+                  "Kandersteg, Switzerland",
+                  style: new TextStyle(color: Colors.grey[500]),
+                )
+              ],
+            ),
+          ),
+          new Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
+          new Text("41")
+        ],
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
+    Color color = Theme.of(context).primaryColor;
+    // 按钮
+    Widget buttonSection = new Container(
+      child: new Row(
+        children: <Widget>[
+          new Expanded(
+            child: new Column(
+              children: <Widget>[
+                new Container(child: new Icon(Icons.call, color: color),margin: const EdgeInsets.only(bottom: 5.0)),
+                new Text("CALL", style: new TextStyle(color: color))
+              ],
+            ),
+          ),
+          new Expanded(
+            child: new Column(
+              children: <Widget>[
+                new Container(child: new Icon(Icons.near_me, color: color),margin: const EdgeInsets.only(bottom: 5.0)),
+                new Text("ROUTE", style: new TextStyle(color: color))
+              ],
+            ),
+          ),
+          new Expanded(
+            child: new Column(
+              children: <Widget>[
+                new Container(child: new Icon(Icons.share, color: color),margin: const EdgeInsets.only(bottom: 5.0)),
+                new Text("SHARE", style: new TextStyle(color: color))
+              ],
+            ),
+          )
+        ],
       ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+    );
+    Widget contentWiget = new Container(
+      padding: const EdgeInsets.all(32.0),
+      child: new Text(
+        "Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.",
+        style: new TextStyle(fontWeight: FontWeight.bold),
+      ),
+    );
+    // 基础，最底层视图
+    MaterialApp materialApp = new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          backgroundColor: Colors.cyan,
+          title: new Text(
+            "Interact",
+            style: new TextStyle(color: Colors.white, fontSize: 32.0),
+          ),
+        ),
+        body: new ListView(
+          children: <Widget>[image, titleSection, buttonSection, contentWiget],
         ),
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+    return materialApp;
   }
 }
