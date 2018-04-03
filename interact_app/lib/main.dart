25ba151ae1505e40d28697bc444eac76dfd1e215
zhoupengzu <zhoupengzu@maizijf.com>
2018-04-03 17:35:19 +0800

    修改展示内容

--- interact_app/lib/main.dart
+++ interact_app/lib/main.dart
@@ -91,6 +91,7 @@ class _MyHomeState extends State<_MyHomeWidget> {
       child: new Text(
         "Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.",
         style: new TextStyle(fontWeight: FontWeight.bold),
+        softWrap: true,
       ),
     );
     // 基础，最底层视图
